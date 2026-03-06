97cc767f41685996f095f71db425d07a426910e8
Lee_SM <gmlsenfn34@naver.com>
2026-03-07 06:48:18 +0900

    Add GroundCheck For Jump

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,26 +2,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 10f;
-
     private Rigidbody2D rb;
     private float moveInput;
+
+    [Header("Move")]
+    public float moveSpeed = 5f;
+    [Header("Jump")]
+    public float jumpForce = 10f;
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
     private bool isGrounded;
 
-    void Awake()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            Jump();
-        }
+        Move();
+        Jump();
     }
 
     void FixedUpdate()
@@ -31,12 +34,28 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        float move = Input.GetAxisRaw("Horizontal");
+
+        rb.linearVelocity = new Vector2(move * moveSpeed, rb.linearVelocity.y);
     }
 
     void Jump()
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        CheckGround();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundCheckRadius,
+            groundLayer
+        );
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -54,4 +73,12 @@ public class PlayerController : MonoBehaviour
             isGrounded = false;
         }
     }
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck == null) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+    
 }
